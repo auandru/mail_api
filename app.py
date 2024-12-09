84b3c0d1b1aa3b6df62a7f20111605c3eaab00e4
auandru <a_andru@ukr.net>
2024-12-09 18:39:08 +0200

    add charset=utf-8

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from flask import Flask, redirect, url_for, session, request, Response, json, jsonify
+from flask import Flask, redirect, url_for, session, request, Response, jsonify
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
-from email.header import decode_header
 import os
 
 app = Flask(__name__)
 app.secret_key = 'a8e864939aac48c7d441781a9568668b'
-
+app.config['JSON_AS_ASCII'] = False
+URL_CALLBACK = 'http://localhost:5000/callback'
 CREDENTIALS_FILE = 'web_google.json'
-
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+MAX_EMAILS = 50
 
 flow = Flow.from_client_secrets_file(
     CREDENTIALS_FILE,
     scopes=SCOPES,
-    redirect_uri='http://localhost:5000/callback'
+    redirect_uri= URL_CALLBACK
 )
 
 
@@ -37,14 +37,7 @@ def authorize():
 def callback():
     flow.fetch_token(authorization_response=request.url)
     credentials = flow.credentials
-    session['credentials'] = {
-        'token': credentials.token,
-        'refresh_token': credentials.refresh_token,
-        'token_uri': credentials.token_uri,
-        'client_id': credentials.client_id,
-        'client_secret': credentials.client_secret,
-        'scopes': credentials.scopes
-    }
+    session['credentials'] = credentials_to_dict(credentials)
     return redirect(url_for('read_emails'))
 
 
@@ -57,7 +50,7 @@ def read_emails():
     service = build('gmail', 'v1', credentials=credentials)
 
     try:
-        results = service.users().messages().list(userId='me', maxResults=50).execute()
+        results = service.users().messages().list(userId='me', maxResults=MAX_EMAILS).execute()
         messages = results.get('messages', [])
         if not messages:
             return jsonify({"message": "No messages found"})
@@ -69,11 +62,8 @@ def read_emails():
             headers = payload.get('headers', [])
 
             subject = next((header['value'] for header in headers if header['name'] == 'Subject'), 'No Subject')
-
             subjects.append(subject)
-
-        session['credentials'] = credentials_to_dict(credentials)
-        return jsonify(subjects)
+        return Response(response= '\n'.join(subjects), status=200, content_type='text/plain; charset=utf-8')
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
